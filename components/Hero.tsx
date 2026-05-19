2c5701bea028e77a43088c1456d1af05d8dbec49
Yash Kumar <yashk40491@gmail.com>
2026-05-19 23:52:50 +0530

    Added new projects and Updated Resume

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
 
   const handleResumeClick = () => {
     // Opens resume.pdf from public folder
-    window.open('/resume.pdf', '_blank');
+    window.open('/Yash_Resume.pdf', '_blank');
   };
 
   return (
